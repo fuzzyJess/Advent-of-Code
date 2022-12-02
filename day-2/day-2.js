92cf4b705b31d108d39eb8a3dd808a352aa84351
fuzzyJess <jrundle99@yahoo.com>
2022-12-02 14:49:57 +0000

    set up test file and js for day 2 problem

---
+++ day-2/day-2.js
@@ -0,0 +1,17 @@
+/*
+A for Rock, B for Paper, and C for Scissors
+X for Rock, Y for Paper, and Z for Scissors
+
+score 1 for Rock, 2 for Paper, and 3 for Scissors
++
+score 0 if you lost, 3 if the round was a draw, and 6 if you won
+
+What would your total score be if everything goes exactly 
+according to your strategy guide?
+*/
+
+function tournamentScore(scores) {
+    
+}
+
+exporting: module.exports = {tournamentScore};
