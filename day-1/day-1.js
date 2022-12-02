239333b0acb37cab7369a61bba02286cd116f670
fuzzyJess <jrundle99@yahoo.com>
2022-12-02 14:14:21 +0000

    create array turning puzzle input into array of moves

---
+++ day-1/day-1.js
@@ -0,0 +1,16 @@
+function heaviestElf(elfArray) {
+    const totals = [];
+    for (let i = 0; i < elfArray.length; i++){
+        let elfTotal = 0;
+        for (let j = 0; j < elfArray[i].length; j++) {
+            elfTotal += elfArray[i][j];
+        }
+        totals.push(elfTotal);
+        elfTotal = 0;
+    }
+    console.log(totals)
+    return Math.max(...totals);
+}
+
+
+exporting: module.exports = {heaviestElf};
