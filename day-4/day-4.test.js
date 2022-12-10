7e5126f9c5e49b46c422a74594de16bab79f9eec
fuzzyJess <jrundle99@yahoo.com>
2022-12-10 21:04:12 +0000

    refactored input data again to make it easier to use in function

---
+++ day-4/day-4.test.js
@@ -0,0 +1,7 @@
+const {pairsFullyContained} = require('./day-4');
+
+describe("pairsFullyContained", () => {
+    test("returns 0 when passed one pair that doesn't full contain its partner", () => {
+        const pair = [["2-9","9-51"]]
+    })
+})
--- day-4/read-data.js
+++ day-4/read-data.js
@@ -2,11 +2,17 @@ const fs = require('fs');
 
 try {
     const data = fs.readFileSync('./data.txt', 'utf-8');
+    console.log(data)
     let data1 = data.split(/\n/);
     let data2 = [];
     for (let i = 0; i < data1.length; i++) {
-        data2.push(data1[i].split(","));
+        data2.push(data1[i].split(/[-,]/));
     }
+    // splits input data into nested arrays containing 4 strings
+    // start range(elf 1), end range(elf 2)
+
+    // will need to use parseInt() to convert these into numbers
+
     const stringyStrings = JSON.stringify(data2);
     fs.writeFileSync("refactored-data.txt", stringyStrings);
 } catch (err) {
