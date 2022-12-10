c68815d2d3023efb4eecd54967a909bd8799c07c
fuzzyJess <jrundle99@yahoo.com>
2022-12-10 19:49:03 +0000

    function heaviestThreeElves passes test and returns correct answer

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -8,9 +8,25 @@ function heaviestElf(elfArray) {
         totals.push(elfTotal);
         elfTotal = 0;
     }
-    console.log(totals)
     return Math.max(...totals);
 }
 
+function heaviestThreeElves(elfArray) {
+    const totals = [];
+    for (let i = 0; i < elfArray.length; i++){
+        let elfTotal = 0;
+        for (let j = 0; j < elfArray[i].length; j++) {
+            elfTotal += elfArray[i][j];
+        }
+        totals.push(elfTotal);
+        elfTotal = 0;
+    }
+    totals.sort((a,b)=>a-b)
+    console.log(totals)
+    let combinedWeight = totals[totals.length-1] + totals[totals.length-2] + totals[totals.length-3];
+    
+    return combinedWeight;
+}
 
-exporting: module.exports = {heaviestElf};
+
+exporting: module.exports = {heaviestElf, heaviestThreeElves};
