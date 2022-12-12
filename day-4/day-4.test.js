e7ef2b68731dc51f582210c1fcf2aa7cb40c16d2
fuzzyJess <jrundle99@yahoo.com>
2022-12-12 11:55:38 +0000

    function overlappingPairs passing third test

--- day-4/day-4.test.js
+++ day-4/day-4.test.js
@@ -36,4 +36,15 @@ describe("overlappingPairs", () => {
         const pairs = [["2","8","9","51"], ["38","49","37","48"]]
         expect(overlappingPairs(pairs)).toBe(1);
     })
+    test("correct number when passed multiple pairs that fully contain their partners", () => {
+        const pairs = [
+            ["2", "4", "6", "8"],
+            ["2", "3", "4", "5"],
+            ["5", "7", "7", "9"],
+            ["2", "8", "3", "7"],
+            ["6", "6", "4", "6"],
+            ["2", "6", "4", "8"]
+        ]
+        expect(overlappingPairs(pairs)).toBe(4);
+    })
 })
