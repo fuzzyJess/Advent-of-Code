c189442d5c8af4c706cc3a3b133e50ba037d6f23
fuzzyJess <jrundle99@yahoo.com>
2022-12-12 14:54:07 +0000

    function topCrates returns correct answer when passed puzzle example

--- day-5/day-5.test.js
+++ day-5/day-5.test.js
@@ -2,10 +2,24 @@ const {topCrates} = require('./day-5')
 const {crates} = require('./crate-data')
 
 describe("topCrates", () => {
-    
     test("returns letters for top crates when one move instruction passed to function", () => {
         const crates = [["Z", "N"], ["M", "C", "D"], ["P"]];
         const instruction = [["1", "2", "3"]]
         expect(topCrates(crates, instruction)).toEqual("NCD");
+    })
+    test("returns letters for top crates when mulitiple instructions passed to function", () => {
+        const crates = [["Z", "N"], ["M", "C", "D"], ["P"]];
+        const instructions = [["1", "2", "3"], ["1", "2", "1"]]
+        expect(topCrates(crates, instructions)).toEqual("CMD");
+    })
+    test("returns correct letters for crates when passed example instructions", () => {
+        const crates = [["Z", "N"], ["M", "C", "D"], ["P"]];
+        const instructions = [
+            ["1", "2", "1"], 
+            ["3", "1", "3"],
+            ["2", "2", "1"],
+            ["1", "1", "2"]
+        ]
+        expect(topCrates(crates, instructions)).toEqual("CMZ")
     })
 })
