2df3d5e7aa683626ff3f97665faa097f31df6e02
fuzzyJess <jrundle99@yahoo.com>
2022-12-12 14:14:10 +0000

    added in comment to explain restructuring of puzzle input

--- day-5/read-data.js
+++ day-5/read-data.js
@@ -8,6 +8,14 @@ try {
         var matches = data1[i].match(/(\d+)/g);
         data2.push(matches)
     }
+    // will need to use parseInt() to convert these into numbers
+
+    // splits input up into nested arrays containing number values 
+    // for each instruction
+    // move = [0]
+    // from = [1]
+    // to = [2]
+    
     const stringyStrings = JSON.stringify(data2);
     fs.writeFileSync("refactored-data.txt", stringyStrings);
 } catch (err) {
